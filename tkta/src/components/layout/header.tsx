4abe6448202ceffc4d94beff9bbf23d0f0abe755
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-01-08 17:21:09 +0400

    Add video and services sections

--- tkta/src/components/layout/header.tsx
+++ tkta/src/components/layout/header.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
-import Logo from "@/assets/logo.png";
+import Logo from "@/assets/images/logo.png";
 import { Navigation } from "./navigation";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex flex-row items-center justify-center gap-[60px] py-4 px-28 font-helvetica">
-      <Image src={Logo} alt="TKTA logo" className="w-[194.4px] h-18"/>
+    <header className="bg-white flex flex-row items-center justify-center gap-8 py-6 px-28 font-helvetica sticky top-0 left-0 z-50">
+      <Link href="/" className="hover:cursor-pointer">
+        <Image src={Logo} alt="TKTA logo" className="w-[194.4px] h-18" />
+      </Link>
 
       <Navigation />
     </header>

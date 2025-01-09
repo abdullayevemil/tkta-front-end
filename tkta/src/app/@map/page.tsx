e6644e35cbb70b0abfb30c6dc5ee0fb241543e9d
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-01-09 22:14:16 +0400

    Add google map

---
+++ tkta/src/app/@map/page.tsx
@@ -0,0 +1,16 @@
+import GoogleMapComponent from "@/components/maps/google-map";
+import { Card } from "@/components/ui/card";
+
+export default function Map() {
+  return (
+    <div className="flex flex-col gap-20 w-full items-end px-[112px]">
+      <h2 className="text-textPrimary font-bold text-4xl w-full justify-center text-center uppercase">
+        Xəbərlər
+      </h2>
+
+      <Card className="h-96 w-1/3 flex justify-right">
+        <GoogleMapComponent />
+      </Card>
+    </div>
+  );
+}
--- tkta/src/app/@news/page.tsx
+++ tkta/src/app/@news/page.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/components/ui/carousel";
 import Link from "next/link";
 
-export default function Services() {
+export default function News() {
   return (
     <div className="flex flex-col gap-20 w-full justify-center">
       <h2 className="text-textPrimary font-bold text-4xl w-full justify-center text-center uppercase">

1b2c22eeb5df0213bde55fcfe47c58123180383d
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-01-10 23:53:31 +0400

    Add agency structure

--- tkta/src/app/agency/about/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomeLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <main className="flex flex-col gap-20 h-full w-full">{children}</main>;
-}

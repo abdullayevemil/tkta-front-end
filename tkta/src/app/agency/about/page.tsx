3492f7d445a36a69d72fe33f993135a04b78fdd1
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-01-10 23:12:21 +0400

    Add about page

---
+++ tkta/src/app/agency/about/page.tsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import HeaderImage from "@/assets/images/about/header-image.png";
+import CenterImage from "@/assets/images/about/center-image.png";
+import Link from "next/link";
+import { Card } from "@/components/ui/card";
+
+export default function AboutUs() {
+  return (
+    <div className="w-full flex flex-col gap-16 items-center">
+      <Image src={HeaderImage} alt="header image" className="w-full" />
+
+      <h1 className="uppercase text-7xl text-center">HAQQIMIZDA</h1>
+
+      <h2 className="italic text-3xl text-center w-2/3 p-4 text-textPrimary">
+        <b>“</b>Təhsil, yalnız bilik əldə etmək deyil, həm də insanın düşünmə
+        qabiliyyətini inkişaf etdirmək, həyatda doğru qərarlar vermək və
+        cəmiyyətə faydalı olmaq üçün ən güclü vasitədir<b>”</b>
+      </h2>
+
+      <div className="flex flex-row px-[112px] gap-16">
+        <Card className="w-2/3">
+          <Image src={CenterImage} alt="center image" className="w-full h-full rounded-xl" />
+        </Card>
+
+        <div className="flex flex-col gap-6 w-1/3 justify-between">
+          <div className="text-base w-full text-justify">
+            Azərbaycan Respublikası Prezidentinin 29.12.2019-cu il tarixdə
+            imzaladığı müvafiq Fərmana əsasən, Təhsil Nazirliyinin Akkreditasiya
+            və Nostrifikasiya İdarəsi Təhsil Nazirliyinin tabeliyində publik
+            hüquqi şəxs statusu olan Təhsildə Keyfiyyət Təminatı Agentliyinə
+            çevrilmə yolu ilə yenidən təşkil edilmişdir. Agentlik Azerbaycanda
+            və beynəlxalq səviyyədə qəbul edilmiş standartlara əsaslanan ali,
+            orta ixtisas və əlavə təhsilin keyfiyyətinin müstəqil
+            qiymətləndirilməsi sahəsində peşəkar və mütəxəssis qurumdur.
+          </div>
+
+          <div className="text-base w-full text-justify">
+            Təhsildə Keyfiyyət Təminatı Agentliyi (TKTA) təhsil proqramlarının
+            keyfiyyət səviyyələrinin müəyyənləşdirilməsi və tələblərin həyata
+            keçirilməsini təmin edir. Agentlik öz vəzifələrini yerinə yetirərkən
+            və hüquqlarını həyata keçirərkən dövlət orqanları (qurumları) və
+            bələdiyyələrlə, beynəlxalq və qeyri-hökumət təşkilatları, digər
+            hüquqi və fiziki şəxslərlə qarşılıqlı əlaqədə fəaliyyət göstərir.
+          </div>
+        </div>
+      </div>
+
+      <div className="text-base w-full px-[112px] text-justify">
+        Azərbaycan Respublikası Təhsil Nazirliyinin fəaliyyətinin
+        təkmilləşdirilməsi və “Azərbaycan Respublikasında bir sıra dövlət
+        orqanlarının strukturunun optimallaşdırılması və idarə edilməsinin
+        təkmilləşdirilməsi ilə bağlı əlavə tədbirlər haqqında” Azərbaycan
+        Respublikası Prezidentinin 2019-cu il 17 yanvar tarixli 479 nömrəli
+        Fərmanında dəyişiklik edilməsi barədə{" "}
+        <Link
+          className="italic text-textPrimary font-semibold"
+          href="https://e-qanun.az/framework/44056"
+        >
+          NİZAMNAMƏYƏ KEÇİD
+        </Link>
+      </div>
+    </div>
+  );
+}
--- tkta/src/app/home/layout.tsx
+++ tkta/src/app/home/layout.tsx
@@ -2,14 +2,18 @@ export default function HomeLayout({
   children,
   video,
   map,
+  statistics,
 }: Readonly<{
   children: React.ReactNode;
   video: React.ReactNode;
   map: React.ReactNode;
+  statistics: React.ReactNode,
 }>) {
   return (
     <main className="flex flex-col gap-20 h-full w-full">
       <section>{video}</section>
+
+      <section>{statistics}</section>
 
       <section>{map}</section>
 

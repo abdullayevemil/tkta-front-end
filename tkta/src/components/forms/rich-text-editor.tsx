c32418773a319307388edc08eaf8c594be5b8c0b
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-01-11 18:26:55 +0400

    Add text aligning toggles

--- tkta/src/components/forms/rich-text-editor.tsx
+++ tkta/src/components/forms/rich-text-editor.tsx
@@ -3,13 +3,17 @@ import {
   Italic,
   Underline,
   Strikethrough,
+  AlignLeft,
+  AlignCenter,
+  AlignRight,
+  AlignJustify,
 } from "lucide-react";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { Card } from "../ui/card";
 
 export default function RichTextEditor() {
   return (
-    <Card>
+    <Card className="flex flex-row gap-1">
       <ToggleGroup type="multiple" variant={"outline"}>
         <ToggleGroupItem
           className="Toggle"
@@ -32,6 +36,27 @@ export default function RichTextEditor() {
           aria-label="Toggle strikethrough"
         >
           <Strikethrough className="h-4 w-4" />
+        </ToggleGroupItem>
+      </ToggleGroup>
+
+      <ToggleGroup type="single" variant={"outline"}>
+        <ToggleGroupItem value="align-left" aria-label="Toggle align left">
+          <AlignLeft className="h-4 w-4" />
+        </ToggleGroupItem>
+
+        <ToggleGroupItem value="align-center" aria-label="Toggle align center">
+          <AlignRight className="h-4 w-4" />
+        </ToggleGroupItem>
+
+        <ToggleGroupItem value="align-right" aria-label="Toggle align right">
+          <AlignCenter className="h-4 w-4" />
+        </ToggleGroupItem>
+
+        <ToggleGroupItem
+          value="align-justify"
+          aria-label="Toggle align justify"
+        >
+          <AlignJustify className="h-4 w-4" />
         </ToggleGroupItem>
       </ToggleGroup>
     </Card>

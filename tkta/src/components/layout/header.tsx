f210879ee1f08f8d182b61a1d44c5385d99014c7
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-01-11 14:39:25 +0400

    Fix bug in header

--- tkta/src/components/layout/header.tsx
+++ tkta/src/components/layout/header.tsx
@@ -7,14 +7,16 @@ import { LanguageSelector } from "./language-selector";
 export default function Header() {
   return (
     <header className="border-b border-gray-100 bg-white flex flex-col items-center justify-center gap-8 py-6 px-16 font-helvetica sticky top-0 left-0 z-50">
-      <Link
-        href="/home"
-        className="hover:cursor-pointer w-full flex justify-between h-full"
-      >
-        <Image src={Logo} alt="TKTA logo" className="w-[194.4px] h-18" />
-
+      <div className="w-full flex justify-between h-full">
+        <Link
+          href="/home"
+          className="hover:cursor-pointer"
+        >
+          <Image src={Logo} alt="TKTA logo" className="w-[194.4px] h-18" />
+        </Link>
+        
         <LanguageSelector />
-      </Link>
+      </div>
 
       <Navigation />
     </header>

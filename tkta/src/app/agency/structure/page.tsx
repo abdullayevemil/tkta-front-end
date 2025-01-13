e759f4a27e79f0c4d3a1e05fcd57f059c65aa512
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-01-13 16:00:53 +0400

    Update structure

--- tkta/src/app/agency/structure/page.tsx
+++ tkta/src/app/agency/structure/page.tsx
@@ -6,58 +6,48 @@ import { ReactFlow } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 
 const initialNodes = [
-  { id: "1", position: { x: 700, y: 0 }, data: { label: "İdarə Heyəti" } },
-  { id: "2", position: { x: 300, y: 100 }, data: { label: "İH-nin sədri" } },
+  { id: "1", position: { x: 700, y: 0 }, data: { label: "İdarə Heyətinin Sədri" } },
+  { id: "2", position: { x: 1100, y: 100 }, data: { label: "İdarə Heyətinin sədr müavini" } },
   {
     id: "3",
-    position: { x: 500, y: 100 },
-    data: { label: "İH-nin sədr müavini" },
+    position: { x: 200, y: 300 },
+    data: { label: "İnformasiya və İnsan resursları şöbəsi" },
   },
   {
     id: "4",
-    position: { x: 700, y: 100 },
-    data: { label: "Keyfiyyət təminatı departamenti" },
+    position: { x: 400, y: 300 },
+    data: { label: "Akkreditasiya və lisenziya şöbəsi" },
   },
   {
     id: "5",
-    position: { x: 900, y: 100 },
-    data: { label: "Hüquq təminatı departamenti" },
+    position: { x: 600, y: 300 },
+    data: { label: "Təlim və metodalogiya şöbəsi" },
   },
   {
     id: "6",
-    position: { x: 1100, y: 100 },
+    position: { x: 800, y: 300 },
     data: { label: "Kvalifikasiyaların tanınması departamenti" },
   },
   {
     id: "7",
-    position: { x: 200, y: 200 },
-    data: { label: "İnformasiya və İnsan resursları şöbəsi" },
+    position: { x: 700, y: 500 },
+    data: { label: "Xarici kvalifikasiyaların tanınması şöbəsi" },
   },
   {
     id: "8",
-    position: { x: 400, y: 200 },
-    data: { label: "Analitik təhlil şöbəsi" },
+    position: { x: 900, y: 500 },
+    data: { label: "Qeyri formal və informal təhsilin tanınması şöbəsi" },
   },
-  { id: "9", position: { x: 600, y: 200 }, data: { label: "Maliyyə şöbəsi" } },
+  { id: "9", position: { x: 1050, y: 300 }, data: { label: "Hüquq təminatı departamenti" } },
   {
     id: "10",
-    position: { x: 700, y: 300 },
-    data: { label: "Akkreditasiya və lisenziya şöbəsi" },
+    position: { x: 1100, y: 500 },
+    data: { label: "Hüquqı məsələlərin tənzimlənməsi şöbəsi" },
   },
   {
     id: "11",
-    position: { x: 900, y: 300 },
-    data: { label: "Təlim və metodologiya şöbəsi" },
-  },
-  {
-    id: "12",
-    position: { x: 1000, y: 200 },
-    data: { label: "Hüquqi məsələlərin tənzimlənməsi şöbəsi" },
-  },
-  {
-    id: "13",
-    position: { x: 1200, y: 200 },
-    data: { label: "Kvalifikasiyaların tanınması departamenti" },
+    position: { x: 1300, y: 300 },
+    data: { label: "Maliyyə şöbəsi" },
   },
 ];
 
@@ -67,13 +57,11 @@ const initialEdges = [
   { id: "e1-4", source: "1", target: "4" },
   { id: "e1-5", source: "1", target: "5" },
   { id: "e1-6", source: "1", target: "6" },
-  { id: "e2-7", source: "2", target: "7" },
-  { id: "e2-8", source: "2", target: "8" },
-  { id: "e3-9", source: "3", target: "9" },
-  { id: "e4-10", source: "4", target: "10" },
-  { id: "e4-11", source: "4", target: "11" },
-  { id: "e5-12", source: "5", target: "12" },
-  { id: "e6-13", source: "6", target: "13" },
+  { id: "e1-9", source: "1", target: "9" },
+  { id: "e1-11", source: "1", target: "11" },
+  { id: "e6-7", source: "6", target: "7" },
+  { id: "e6-8", source: "6", target: "8" },
+  { id: "e9-10", source: "9", target: "10" },
 ];
 
 export default function Structure() {

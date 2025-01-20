9a437256eb0dc82fe71e11bc47042a9b0d37f6e9
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-01-20 14:35:57 +0400

    Add font selector

--- tkta/src/components/layout/header.tsx
+++ tkta/src/components/layout/header.tsx
@@ -3,19 +3,21 @@ import Logo from "@/assets/images/logo.png";
 import { Navigation } from "./navigation";
 import Link from "next/link";
 import { LanguageSelector } from "./language-selector";
+import { FontSelector } from "./font-selector";
 
 export default function Header() {
   return (
     <header className="border-b border-gray-100 bg-white flex flex-col items-center justify-center gap-8 py-6 px-16 font-helvetica sticky top-0 left-0 z-50">
       <div className="w-full flex justify-between h-full">
-        <Link
-          href="/home"
-          className="hover:cursor-pointer"
-        >
+        <Link href="/home" className="hover:cursor-pointer">
           <Image src={Logo} alt="TKTA logo" className="w-[194.4px] h-18" />
         </Link>
-        
-        <LanguageSelector />
+
+        <div className="flex flex-col gap-2">
+          <LanguageSelector />
+
+          <FontSelector />
+        </div>
       </div>
 
       <Navigation />

0b913824487a13682dbec6b2a73d698b373ac5c8
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-01-30 09:39:14 +0400

    Add not recognized universities page

--- tkta/src/app/not-recognized-universities/page.tsx
+++ tkta/src/app/not-recognized-universities/page.tsx
@@ -36,335 +36,241 @@ import {
 
 interface University {
   id: number;
-  country: string;
   name: string;
-  rejectionPercentage: number;
+  country: string;
+  status: string;
 }
 
 const data: University[] = [
   {
     id: 1,
-    country: "Gürcüstan",
-    name: "Avropa Tədris Universiteti",
-    rejectionPercentage: 100,
+    name: "Beynəlxalq Atlantik Universiteti",
+    country: "Amerika Birləşmiş Ştatları",
+    status: "Yoxdur",
   },
   {
     id: 2,
-    country: "Ukrayna",
-    name: "Şərqi Avropa İqtisadiyyat və İdarəetmə Universiteti",
-    rejectionPercentage: 99,
+    name: "Beynəlxalq Xristian Universiteti",
+    country: "Avstriya Respublikası",
+    status: "Yoxdur (Hazırda fəaliyyət göstərmir)",
   },
   {
     id: 3,
-    country: "Gürcüstan",
-    name: "Gürcüstan Yeni Universiteti",
-    rejectionPercentage: 98,
+    name: "Middlesex Universiteti",
+    country: "Birləşmiş Ərəb Əmirlikləri",
+    status: "Yoxdur",
   },
   {
     id: 4,
-    country: "Ukrayna",
-    name: "Avqustin Voloşin adına Karpat Universiteti",
-    rejectionPercentage: 93,
+    name: "Beynəlxalq Praqa Universiteti",
+    country: "Çex Respublikası",
+    status: "Yoxdur",
   },
   {
     id: 5,
-    country: "Rusiya Federasiyası",
-    name: "Sankt-Peterburq Xarici İqtisadi Əlaqələr, İqtisadiyyat və Hüquq İnstitutu",
-    rejectionPercentage: 92,
+    name: "EICAR – Paris Beynəlxalq Film və Televiziya Məktəbi",
+    country: "Fransa Respublikası",
+    status: "Yoxdur",
   },
   {
     id: 6,
-    country: "Rusiya Federasiyası",
-    name: "Cənubi Rusiya Humanitar İnstitutu",
-    rejectionPercentage: 86,
+    name: "Vatel Otel və Turizm Biznes Məktəbi",
+    country: "Fransa Respublikası",
+    status: "Yoxdur",
   },
   {
     id: 7,
-    country: "Rusiya Federasiyası",
-    name: "Dərbənd Humanitar İnstitutu",
-    rejectionPercentage: 85,
+    name: "Şərqi Avropa İqtisadiyyat və İdarəetmə Universiteti",
+    country: "Ukrayna",
+    status: "Gürcüstan filialının akkreditasiyası yoxdur",
   },
   {
     id: 8,
-    country: "Rusiya Federasiyası",
-    name: "Müasir Humanitar Akademiya",
-    rejectionPercentage: 83,
+    name: "Laqodexi Elm-Təhsil İnstitutu",
+    country: "Ukrayna",
+    status: "Yoxdur (Hazırda fəaliyyət göstərmir)",
   },
   {
     id: 9,
-    country: "Rusiya Federasiyası",
-    name: "Sankt-Peterburq Dövlət Mülki Aviasiya Universiteti",
-    rejectionPercentage: 81,
+    name: "Beynəlxalq Ekologiya və Tibb Akademiyası",
+    country: "Ukrayna",
+    status: "Gürcüstan filialının akkreditasiyası yoxdur",
   },
   {
     id: 10,
-    country: "Rusiya Federasiyası",
-    name: "Dünya İqtisadiyyatı İnstitutu",
-    rejectionPercentage: 79,
+    name: "Avqustin Voloşin adına Karpat Universiteti",
+    country: "Ukrayna",
+    status: "Gürcüstan filialının akkreditasiyası yoxdur",
   },
   {
     id: 11,
-    country: "Rusiya Federasiyası",
-    name: "Dağıstan Təhsil və Mədəniyyət Akademiyası",
-    rejectionPercentage: 78,
+    name: "Avropa Universiteti",
+    country: "Ukrayna",
+    status: "Gürcüstan filialının akkreditasiyası yoxdur",
   },
   {
     id: 12,
-    country: "Rusiya Federasiyası",
-    name: "İvanovo Dövlət Universiteti",
-    rejectionPercentage: 76,
+    name: "KROK İqtisadiyyat və Hüquq Universiteti",
+    country: "Ukrayna",
+    status: "Gürcüstan filialının akkreditasiyası yoxdur",
   },
   {
     id: 13,
-    country: "Gürcüstan",
-    name: "Avropa Universiteti",
-    rejectionPercentage: 76,
+    name: "Regionlararası Heyətin İdarəolunması Akademiyası",
+    country: "Ukrayna",
+    status: "Gürcüstan filialının akkreditasiyası yoxdur",
   },
   {
     id: 14,
-    country: "Gürcüstan",
-    name: "Millenium Tədris Universiteti",
-    rejectionPercentage: 73,
+    name: "EADA Biznes Məktəbi",
+    country: "İspaniya Krallığı",
+    status: "Yoxdur",
   },
   {
     id: 15,
-    country: "Gürcüstan",
-    name: "Gürcüstan Beynəlxalq Tədris Universiteti",
-    rejectionPercentage: 72,
+    name: "Barselona Qlobal Biznes Məktəbi",
+    country: "İspaniya Krallığı",
+    status: "Yoxdur",
   },
   {
     id: 16,
-    country: "Ukrayna",
-    name: "Avropa Universiteti",
-    rejectionPercentage: 71,
+    name: "Avropa Universiteti Biznes Məktəbi",
+    country: "İspaniya Krallığı",
+    status: "Yoxdur",
   },
   {
     id: 17,
-    country: "Gürcüstan",
-    name: "Tbilisi Açıq Tədris Universiteti",
-    rejectionPercentage: 66,
+    name: "Marbella Beynəlxalq Universiteti",
+    country: "İspaniya Krallığı",
+    status: "Yoxdur",
   },
   {
     id: 18,
-    country: "Rusiya Federasiyası",
-    name: "Moskva Sahibkarlıq və Hüquq İnstitutu",
-    rejectionPercentage: 66,
+    name: "Harbour Space Universiteti",
+    country: "İspaniya Krallığı",
+    status: "Yoxdur",
   },
   {
     id: 19,
-    country: "Rusiya Federasiyası",
-    name: "Rusiya Dövlət Turizm və Xidmət Universiteti",
-    rejectionPercentage: 60,
+    name: "Roma Biznes Məktəbi",
+    country: "İtaliya Respublikası",
+    status: "Yoxdur",
   },
   {
     id: 20,
-    country: "Rusiya Federasiyası",
-    name: "Cənub Federal Universiteti",
-    rejectionPercentage: 60,
+    name: "Maranqoni İnstitutu",
+    country: "İtaliya Respublikası",
+    status: "Yoxdur",
   },
   {
     id: 21,
-    country: "Ukrayna",
-    name: "T.Şevçenko adına Luqansk Milli Universiteti",
-    rejectionPercentage: 58,
+    name: "Beynəlxalq Humanitar-Texniki Akademiya",
+    country: "Rusiya Federasiyası",
+    status: "26.04.2015-ci ilə qədər akkreditasiyası var",
   },
   {
     id: 22,
+    name: "Nijeqorod Menecment və Biznes İnstitutu",
     country: "Rusiya Federasiyası",
-    name: "Sankt Peterburq Dövlət Mülki Aviasiya Universiteti",
-    rejectionPercentage: 58,
+    status: "26.04.2015-ci ilə qədər akkreditasiyası var",
   },
   {
     id: 23,
+    name: "Şərqi Avropa Psixoanaliz İnstitutu",
     country: "Rusiya Federasiyası",
-    name: "Maliyyə və Hüquq İnstitutu",
-    rejectionPercentage: 55,
+    status: "22.06.2017-ci ilə qədər akkreditasiyası var",
   },
   {
     id: 24,
-    country: "Ukrayna",
-    name: "Milli Aerokosmik Universiteti 'Xarkov Aviasiya İnstitutu'",
-    rejectionPercentage: 55,
+    name: "Sosial-Humanitar Təhsil və Xidmət İnstitutu",
+    country: "Rusiya Federasiyası",
+    status:
+      "Ali təhsil müəssisəsi 19.12.2017-ci il tarixində fəaliyyətini dayandırıb",
   },
   {
     id: 25,
+    name: "Dünya İqtisadiyyatı İnstitutu",
     country: "Rusiya Federasiyası",
-    name: "Dərbənd İncəsənət və Mədəniyyət İnstitutu",
-    rejectionPercentage: 55,
+    status:
+      "2014-cü ilə qədər olub və 2024-cü ildən təkrar akkreditasiyası var",
   },
   {
     id: 26,
-    country: "Ukrayna",
-    name: "Poltava İqtisadiyyat və Ticarət Universiteti",
-    rejectionPercentage: 53,
+    name: "Moskva Humanitar-İqtisad İnstitutu",
+    country: "Rusiya Federasiyası",
+    status: "Ukrayna filialının akkreditasiyası yoxdur",
   },
   {
     id: 27,
-    country: "Gürcüstan",
-    name: "Gürcüstan Kənd Təsərrüfatı Universiteti",
-    rejectionPercentage: 52,
+    name: "İsveçrədə Amerika Tətbiqi Elmlər İnstitutu",
+    country: "İsveçrə Konfederasiyası",
+    status: "Yoxdur",
   },
   {
     id: 28,
-    country: "Rusiya Federasiyası",
-    name: "'Sinergiya' Moskva Maliyyə-Sənaye Universiteti",
-    rejectionPercentage: 52,
+    name: "İsveçrə Ali Təhsil Məktəbi",
+    country: "İsveçrə Konfederasiyası",
+    status: "Yoxdur",
   },
   {
     id: 29,
-    country: "Gürcüstan",
-    name: "Tbilisi Humanitar Tədris Universiteti",
-    rejectionPercentage: 51,
+    name: "Avropa Universiteti Biznes Məktəbi",
+    country: "İsveçrə Konfederasiyası",
+    status: "Yoxdur",
   },
   {
     id: 30,
-    country: "Rusiya Federasiyası",
-    name: "Moskva Politexnik Universiteti",
-    rejectionPercentage: 51,
+    name: "Cenevrə Biznes Məktəbi",
+    country: "İsveçrə Konfederasiyası",
+    status: "Yoxdur",
   },
   {
     id: 31,
-    country: "Ukrayna",
-    name: "Regionlararası Heyətin İdarəolunması Akademiyası",
-    rejectionPercentage: 50,
+    name: "Cenevrə Beynəlxalq Universiteti",
+    country: "İsveçrə Konfederasiyası",
+    status: "Yoxdur",
   },
   {
     id: 32,
-    country: "Ukrayna",
-    name: "Xarkov Dövlət Qida Texnologiyası və Ticarət Universiteti",
-    rejectionPercentage: 50,
+    name: "Biznes və Hotel Menecmenti Məktəbi",
+    country: "İsveçrə Konfederasiyası",
+    status: "Yoxdur",
   },
   {
     id: 33,
-    country: "Gürcüstan",
-    name: "Suxişvili Tədris Universiteti",
-    rejectionPercentage: 50,
+    name: "Cenevrə Diplomatiya və Beynəlxalq Münasibətlər Universiteti",
+    country: "İsveçrə Konfederasiyası",
+    status: "Yoxdur",
   },
   {
     id: 34,
-    country: "Rusiya Federasiyası",
-    name: "Rusiya Dövlət Sosial Universiteti",
-    rejectionPercentage: 49,
+    name: "İsveçrə Montre Biznes Məktəbi",
+    country: "İsveçrə Konfederasiyası",
+    status: "Yoxdur",
   },
   {
     id: 35,
-    country: "Ukrayna",
-    name: "Milli Aviasiya Universitetinin Uçuş Akademiyası",
-    rejectionPercentage: 48,
+    name: "Biznes və Beynəlxalq Münasibətlər Universiteti",
+    country: "İsveçrə Konfederasiyası",
+    status: "Yoxdur",
   },
   {
     id: 36,
-    country: "Rusiya Federasiyası",
-    name: "Dağıstan Dövlət Universiteti",
-    rejectionPercentage: 47,
+    name: "Beynəlxalq Biznes İnstitutu",
+    country: "Çex Respublikası",
+    status: "Yoxdur",
   },
   {
     id: 37,
-    country: "Ukrayna",
-    name: "'KROK' İqtisadiyyat və Hüquq Universiteti",
-    rejectionPercentage: 46,
+    name: "UniMan Beynəlxalq İdarəetmə və Performans Akademiyası",
+    country: "Fransa Respublikası",
+    status: "Yoxdur",
   },
   {
     id: 38,
-    country: "Rusiya Federasiyası",
-    name: "Rostov Dövlət İqtisad Universiteti",
-    rejectionPercentage: 46,
-  },
-  {
-    id: 39,
-    country: "Rusiya Federasiyası",
-    name: "Rusiya Təhsil Akademiyasının Smolnı İnstitutu",
-    rejectionPercentage: 45,
-  },
-  {
-    id: 40,
-    country: "Ukrayna",
-    name: "Xarkov Milli Avtomobil - Yol Universiteti",
-    rejectionPercentage: 42,
-  },
-  {
-    id: 41,
-    country: "Rusiya Federasiyası",
-    name: "Moskva Dövlət Avtomobil - Yol Texniki Universiteti",
-    rejectionPercentage: 42,
-  },
-  {
-    id: 42,
-    country: "Rusiya Federasiyası",
-    name: "M.M.Cambulatov adına Dağıstan Dövlət Aqrar Universiteti",
-    rejectionPercentage: 41,
-  },
-  {
-    id: 43,
-    country: "Ukrayna",
-    name: "Sumı Dövlət Universiteti",
-    rejectionPercentage: 41,
-  },
-  {
-    id: 44,
-    country: "Rusiya Federasiyası",
-    name: "Moskva Dövlət Maşınqayırma Universiteti",
-    rejectionPercentage: 41,
-  },
-  {
-    id: 45,
-    country: "Gürcüstan",
-    name: "Qafqaz Beynəlxalq Universiteti",
-    rejectionPercentage: 41,
-  },
-  {
-    id: 46,
-    country: "Rusiya Federasiyası",
-    name: "Dağıstan Dövlət Pedaqoji Universiteti",
-    rejectionPercentage: 40,
-  },
-  {
-    id: 47,
-    country: "Rusiya Federasiyası",
-    name: "Rusiya Dövlət Humanitar Universiteti",
-    rejectionPercentage: 36,
-  },
-  {
-    id: 48,
-    country: "Rusiya Federasiyası",
-    name: "Moskva Humanitar-İqtisad İnstitutu",
-    rejectionPercentage: 35,
-  },
-  {
-    id: 49,
-    country: "Ukrayna",
-    name: "Mariupol Dövlət Universiteti",
-    rejectionPercentage: 32,
-  },
-  {
-    id: 50,
-    country: "Ukrayna",
-    name: "Kiyev Milli Texnologiyalar və Dizayn Universiteti",
-    rejectionPercentage: 31,
-  },
-  {
-    id: 51,
-    country: "Rusiya Federasiyası",
-    name: "Cəzaların İcrası Federal Xidmətinin Hüquq və İdarəetmə Akademiyası",
-    rejectionPercentage: 31,
-  },
-  {
-    id: 52,
-    country: "Ukrayna",
-    name: "O.M.Beketov adına Xarkov Milli Şəhər İqtisadiyyatı Universiteti",
-    rejectionPercentage: 31,
-  },
-  {
-    id: 53,
-    country: "Ukrayna",
-    name: "O.Qonçar adına Dnepropetrovsk Milli Universiteti",
-    rejectionPercentage: 30,
-  },
-  {
-    id: 54,
-    country: "Rusiya Federasiyası",
-    name: "Don Dövlət Texniki Universiteti",
-    rejectionPercentage: 30,
+    name: "Biznes, İnnovasiya və Davamlılıq Universiteti",
+    country: "Amerika Birləşmiş Ştatları",
+    status: "Yoxdur",
   },
 ];
 
@@ -397,23 +303,21 @@ const columns: ColumnDef<University>[] = [
     cell: ({ row }) => <div className="capitalize">{row.getValue("name")}</div>,
   },
   {
-    accessorKey: "rejectionPercentage",
+    accessorKey: "status",
     header: ({ column }) => {
       return (
         <Button
           variant="ghost"
-          className="w-full text-center"
+          className="w-full"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          İmtina
+          Status
           <ArrowUpDown />
         </Button>
       );
     },
     cell: ({ row }) => (
-      <div className="lowercase text-center">
-        {row.getValue("rejectionPercentage")}%
-      </div>
+      <div className="capitalize">{row.getValue("status")}</div>
     ),
   },
 ];
@@ -451,18 +355,17 @@ export default function DataTableDemo() {
       <Image src={HeaderImage} alt="header image" className="w-full" />
 
       <h1 className="uppercase text-6xl text-center w-full px-[112px]">
-        İmtina alan universitetlər
+        Tanınmayan universitetlər
       </h1>
 
       <h2 className="text-base text-center px-16">
         <div>
           Təhsildə Keyfiyyət Təminatı Agentliyinə (TKTA) edilən müraciətlərin
-          təhlili nəticəsində Qaydaların 4-cü maddəsində qeyd olunan tanınma
-          üzrə ümumi tələblərə cavab vermədiyi üçün ali təhsil sənədlərinə ən
-          çox imtina verilmiş universitetlərin siyahısı
+          təhlili nəticəsində yerli akkreditasiyası olmadığı üçün ali təhsil
+          sənədlərinə imtina verilmiş universitetlərin siyahısı
         </div>
 
-        <div>Müvafiqdir: 21.05.2024</div>
+        <div>Müvafiqdir: 23.11.2024</div>
       </h2>
 
       <div className="w-full px-16">

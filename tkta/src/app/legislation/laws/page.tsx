8e8db464974b0902b01ec0706454a7c2cdcef5c6
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-01-31 02:16:44 +0400

    Add laws page

---
+++ tkta/src/app/legislation/laws/page.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import Image from "next/image";
+import HeaderImage from "@/assets/images/law/header-image.png";
+import { Card } from "@/components/ui/card";
+import LawIcon from "@/assets/icons/law/law.svg";
+
+const laws = [
+  {
+    id: 1,
+    link: "https://e-qanun.az/framework/18343",
+    title: '"Təhsil haqqında"',
+  },
+  {
+    id: 2,
+    link: "https://e-qanun.az/framework/31994",
+    title: '"Publik hüquqi şəxslər haqqında"',
+  },
+  {
+    id: 3,
+    link: "https://e-qanun.az/framework/2860",
+    title: '"Dövlət rüsumu haqqında"',
+  },
+  {
+    id: 4,
+    link: "https://e-qanun.az/framework/11254",
+    title: '"İnzibati icraat haqqında"',
+  },
+];
+
+export default function Laws() {
+  return (
+    <section className="w-full flex flex-col gap-16 items-center">
+      <Image src={HeaderImage} alt="header image" className="w-full" />
+
+      <h1 className="uppercase text-6xl text-center w-full px-[112px]">
+        Qanunlar
+      </h1>
+
+      <ul className="grid grid-cols-4 gap-4 px-16">
+        {laws.map((law) => (
+          <li key={law.id}>
+            <Link
+              className="hover:text-textPrimary"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={law.link}
+            >
+              <Card className="p-6 py-20 flex flex-col gap-6 items-center justify-center hover:cursor-pointer hover:font-bold text-base">
+                <LawIcon width="30%" color="var(--primary-color)" />
+
+                <div className="text-center">
+                  <div>{law.title}</div>
+
+                  <div> Azərbaycan Respublikasının Qanunu</div>
+                </div>
+              </Card>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}

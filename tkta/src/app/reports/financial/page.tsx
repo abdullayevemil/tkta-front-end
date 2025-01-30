0bf5bc428fb4d1ebc2022e140d9f7a96e3866139
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-01-31 02:41:30 +0400

    Add reports header image

--- tkta/src/app/reports/financial/page.tsx
+++ tkta/src/app/reports/financial/page.tsx
@@ -1,25 +1,39 @@
 import FooterIcon from "@/assets/icons/reports/Folder.svg";
 import { Card } from "@/components/ui/card";
 import Link from "next/link";
+import Image from "next/image";
+import HeaderImage from "@/assets/images/reports/header-image.png";
 
 export default function AnnualReports() {
   return (
-    <section className="px-16 grid grid-cols-4 gap-4 items-center">
-      <Link target="_blank" rel="noopener noreferrer" href="">
-        <Card className="p-6 py-20 flex flex-col gap-6 items-center justify-center hover:cursor-pointer hover:font-bold text-base">
-          <FooterIcon width="30%" color="var(--primary-color)" />
+    <section className="w-full flex flex-col gap-16 items-center">
+      <Image src={HeaderImage} alt="header image" className="w-full" />
 
-          <span className="text-center text-textPrimary">2024</span>
-        </Card>
-      </Link>
+      <h1 className="uppercase text-6xl text-center w-full px-[112px]">
+        Maliyə hesabatlar
+      </h1>
 
-      <Link target="_blank" rel="noopener noreferrer" href="https://tkta.edu.az/files/marsol/forum/qfitt/aylıq/t%27lim/Audit%20r%C9%99yi%202023%20%28sayt%20üçün%29.pdf">
-        <Card className="p-6 py-20 flex flex-col gap-6 items-center justify-center hover:cursor-pointer hover:font-bold text-base">
-          <FooterIcon width="30%" color="var(--primary-color)" />
+      <div className="grid grid-cols-4 px-16 gap-6">
+        <Link target="_blank" rel="noopener noreferrer" href="">
+          <Card className="p-6 py-20 flex flex-col gap-6 items-center justify-center hover:cursor-pointer hover:font-bold text-base">
+            <FooterIcon width="30%" color="var(--primary-color)" />
 
-          <span className="text-center text-textPrimary">2023</span>
-        </Card>
-      </Link>
+            <span className="text-center text-textPrimary">2024</span>
+          </Card>
+        </Link>
+
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://tkta.edu.az/files/marsol/forum/qfitt/aylıq/t%27lim/Audit%20r%C9%99yi%202023%20%28sayt%20üçün%29.pdf"
+        >
+          <Card className="p-6 py-20 flex flex-col gap-6 items-center justify-center hover:cursor-pointer hover:font-bold text-base">
+            <FooterIcon width="30%" color="var(--primary-color)" />
+
+            <span className="text-center text-textPrimary">2023</span>
+          </Card>
+        </Link>
+      </div>
     </section>
   );
 }

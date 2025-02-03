222c6c224718e47b510888592b5b123e81fd8b91
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-02-03 11:04:21 +0400

    Add regulations links

---
+++ tkta/src/app/legislation/agency-regulations/page.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import Image from "next/image";
+import HeaderImage from "@/assets/images/law/header-image.png";
+import { Card } from "@/components/ui/card";
+import LawIcon from "@/assets/icons/law/law.svg";
+
+const laws = [
+  {
+    id: 1,
+    link: "https://e-qanun.az/framework/44056",
+    title:
+      "Təhsildə Keyfiyyət Təminatı Agentliyinin NİZAMNAMƏSİ",
+  },
+];
+
+export default function Laws() {
+  return (
+    <section className="w-full flex flex-col gap-16 items-center">
+      <Image src={HeaderImage} alt="header image" className="w-full" />
+
+      <h1 className="uppercase text-6xl text-center w-full px-[112px]">
+      Agentliyin Nizamnaməsi
+      </h1>
+
+      <ul className="grid grid-cols-4 gap-4 px-16">
+        {laws.map((law) => (
+          <li key={law.id}>
+            <Link
+              className="hover:text-textPrimary"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={law.link}
+            >
+              <Card className="p-6 py-20 flex flex-col gap-6 items-center justify-center hover:cursor-pointer hover:font-semibold hover:text-textPrimary text-base">
+                <LawIcon width="30%" color="var(--primary-color)" />
+
+                <div className="text-center">
+                  <div className="line-clamp-4">{law.title}</div>
+                </div>
+              </Card>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}

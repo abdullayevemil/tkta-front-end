daa0d48ed9ff77406cc931e26310977115711a46
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-02-03 11:21:49 +0400

    Add international accreditations

---
+++ tkta/src/data/international_accreditation_reports.ts
@@ -0,0 +1,57 @@
+export const international_reports = [
+    {
+        id: 1,
+        university: 'AzTU',
+        link: 'https://res.cloudinary.com/dtwyjdkb1/image/upload/v1738566833/accreditation/international/jt5a6lircrtkldchjwz5.pdf',
+    },
+    {
+        id: 2,
+        university: 'AzMIU',
+        link: 'https://res.cloudinary.com/dtwyjdkb1/image/upload/v1738566845/accreditation/international/qmrdaubsk3rwtiram555.pdf',
+    },
+    {
+        id: 3,
+        university: 'ATU',
+        link: 'https://res.cloudinary.com/dtwyjdkb1/image/upload/v1738566831/accreditation/international/j7vkbde7ewykittpztv2.pdf',
+    },
+    {
+        id: 4,
+        university: 'UNEC',
+        link: 'https://res.cloudinary.com/dtwyjdkb1/image/upload/v1738566831/accreditation/international/uc2unarucykztgrhysll.pdf',
+    },
+    {
+        id: 5,
+        university: 'Fəaliyyət Planı',
+        link: 'https://res.cloudinary.com/dtwyjdkb1/image/upload/v1738566830/accreditation/international/yxibm4d7ixdh7tleinod.pdf',
+    },
+    {
+        id: 6,
+        university: 'ADA',
+        link: 'https://res.cloudinary.com/dtwyjdkb1/image/upload/v1738566829/accreditation/international/nvsuwz26yjwhnky1abcs.pdf',
+    },
+    {
+        id: 7,
+        university: 'Xəzər',
+        link: 'https://res.cloudinary.com/dtwyjdkb1/image/upload/v1738566828/accreditation/international/gjnt5ducfatbwqocyqhj.pdf',
+    },
+    {
+        id: 8,
+        university: 'LDU',
+        link: 'https://res.cloudinary.com/dtwyjdkb1/image/upload/v1738566827/accreditation/international/kn7egfrqg8afgztaplsc.pdf',
+    },
+    {
+        id: 9,
+        university: 'BMU',
+        link: 'https://res.cloudinary.com/dtwyjdkb1/image/upload/v1738566827/accreditation/international/rta2a06kmkfmleliwqex.pdf',
+    },
+    {
+        id: 10,
+        university: 'ADPU',
+        link: 'https://res.cloudinary.com/dtwyjdkb1/image/upload/v1738566826/accreditation/international/tqhsjigyjiyroogrywx2.pdf',
+    },
+    {
+        id: 11,
+        university: 'BDU',
+        link: 'https://res.cloudinary.com/dtwyjdkb1/image/upload/v1738566826/accreditation/international/lfo40goo6s4eo3turiz2.pdf',
+    },
+];

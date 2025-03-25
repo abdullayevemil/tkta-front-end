b41df77c26665066acbecaa64cdce636224ce928
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-03-26 01:39:40 +0400

    Fix eslint errors

--- tkta/src/app/home/@partners/page.tsx
+++ tkta/src/app/home/@partners/page.tsx
@@ -7,7 +7,6 @@ import {
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
-import Link from "next/link";
 
 const partners = [
   {

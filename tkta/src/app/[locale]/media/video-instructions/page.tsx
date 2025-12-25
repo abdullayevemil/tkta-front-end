95833750b58fcc5efc219f4eaf3501eeccddd844
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-12-25 19:01:06 +0400

    Add video instructions

--- tkta/src/app/[locale]/media/video-instructions/page.tsx
+++ tkta/src/app/[locale]/media/video-instructions/page.tsx
@@ -1,7 +1,7 @@
 export default function VideoInstructionsPage() {
   return (
-    <div className="container mx-auto px-4 sm:px-8 md:px-16 pt-4 sm:pt-8 md:pt-12 flex flex-col gap-6 items-center text-textPrimary">
-      <h1 className="text-2xl font-bold text-center">
+    <div className="container w-full mx-auto px-4 sm:px-8 md:px-16 pt-4 sm:pt-8 md:pt-12 flex flex-col gap-6 items-center text-textPrimary">
+      <h1 className="text-2xl font-bold text-center w-full">
         Xarici dövlətlərin ali təhsilə aid kvalifikasiyalarının tanınmasına dair
         video-təlimat: Necə müraciət etməliyik?
       </h1>

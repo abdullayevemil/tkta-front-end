8bca5c53aa2f4b4f1abfee65aeea4b728c2a91c0
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-12-25 18:55:00 +0400

    Add video instructions

--- tkta/src/app/[locale]/media/video-instructions/page.tsx
+++ tkta/src/app/[locale]/media/video-instructions/page.tsx
@@ -1,13 +1,13 @@
 export default function VideoInstructionsPage() {
   return (
     <div className="container mx-auto px-4 sm:px-8 md:px-16 pt-4 sm:pt-8 md:pt-12 flex flex-col gap-6 items-center text-textPrimary">
-      <h1 className="text-2xl font-bold">
+      <h1 className="text-2xl font-bold text-center">
         Xarici dövlətlərin ali təhsilə aid kvalifikasiyalarının tanınmasına dair
         video-təlimat: Necə müraciət etməliyik?
       </h1>
 
       <video
-        className="w-full h-auto w-3/5 rounded-lg shadow-lg"
+        className="w-full h-auto w-1/2 rounded-lg shadow-lg"
         controls
         src="https://res.cloudinary.com/dtwyjdkb1/video/upload/v1766674258/WhatsApp_Video_2025-12-25_at_18.17.21_t9dylb.mp4"
       ></video>
